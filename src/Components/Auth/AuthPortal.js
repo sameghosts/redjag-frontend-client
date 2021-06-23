4382f40adc43400b6129ad76354247f786759df7
damatoaj <mr.arthurdamato@gmail.com>
2021-06-23 13:31:44 -0700

    fetching main

--- src/Components/Auth/AuthPortal.js
+++ src/Components/Auth/AuthPortal.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
+//import components
+import Login from './Login.js';
+import Signup from './Signup.js';
+
 export default function AuthPortal() {
     return (
         <div>
+            <Login />
+            <Signup />
         </div>
     )
 }
