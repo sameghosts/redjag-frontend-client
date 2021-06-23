ed1c01cfc8e3605492f4a59a6aa93d2d0ca6593a
damatoaj <mr.arthurdamato@gmail.com>
2021-06-23 11:17:46 -0700

    edited css folders

--- src/Components/Landing/DefaultLanding.js
+++ src/Components/Landing/DefaultLanding.js
@@ -1,10 +1,35 @@
 import React from 'react'
 
+//Import components
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
+
 function DefaultLanding() {
     return (
-        <div>
-            <h1>this is the default landing page</h1>
-        </div>
+        <Container className="landingContainer" fluid>
+            <Row lg={2} md={2} sm={2}>
+                <Col sm={8}>
+                    <Row>
+                        <h2>Hello...</h2>
+                        <h1>Welcome To Tracker</h1>
+                    </Row>
+                    <Row>
+                        <p>Tracker was designed to help job seekers in job searching, job applications, and networking endeavors.</p>
+                    </Row>
+                    <Row>
+                        <Button>
+                            Learn More
+                        </Button>
+                    </Row>
+                </Col>
+                <Col sm={4}>
+                    <h1>An image will go here</h1>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
