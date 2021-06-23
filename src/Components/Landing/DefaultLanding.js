d05807174d0c5027ed145d5a5058f59c06d3bd04
damatoaj <mr.arthurdamato@gmail.com>
2021-06-23 10:49:58 -0700

    fixed a web portal error

--- src/Components/Landing/DefaultLanding.js
+++ src/Components/Landing/DefaultLanding.js
@@ -3,7 +3,7 @@ import React from 'react'
 function DefaultLanding() {
     return (
         <div>
-            
+            <h1>this is the default landing page</h1>
         </div>
     )
 }
--- src/Components/Test/Test.js
+++ src/Components/Test/Test.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import LandingPortal from '../Landing/LandingPortal';
+
 function Test() {
     return (
         <div>
