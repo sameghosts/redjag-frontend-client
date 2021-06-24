10ce722e640768200d1a4829a44d2708c5342a26
Jesse Maretz <jesse.maretz@gmail.com>
2021-06-23 21:55:05 -0400

    imported button and container from bootstrap to profile

--- src/Components/Profile/UserPortal.js
+++ src/Components/Profile/UserPortal.js
@@ -1,5 +1,10 @@
 import React from 'react'
 
+//import bootstrap components
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+
+
 export default function UserPortal() {
     return (
         <div>
