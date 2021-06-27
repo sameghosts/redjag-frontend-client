83d2134c4f2c8369bfb369dd7612ee9b5445af62
Jesse Maretz <jesse.maretz@gmail.com>
2021-06-26 21:51:13 -0400

    created a profile about section

---
+++ src/Components/Profile/ProfileAboutSection.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+//import boostrap tags
+import { Container, Row, Button } from 'react-bootstrap';
+
+export default function () {
+    return (
+        <div>
+            <Container>
+
+            </Container>
+        </div>
+    )
+}
