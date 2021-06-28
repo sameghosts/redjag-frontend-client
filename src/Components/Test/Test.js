4a8adfc4d876450fe64f2ac19c45590124862044
damatoaj <mr.arthurdamato@gmail.com>
2021-06-28 13:07:38 -0700

    changed component tree

--- src/Components/Test/Test.js
+++ src/Components/Test/Test.js
@@ -4,11 +4,13 @@ import AuthPortal from '../Auth/AuthPortal';
 import NavbarPortal from '../Navbar/NavbarPortal';
 import LandingPortal from '../Landing/LandingPortal';
 import TodoPortal from '../Todo/TodoPortal';
+import UserPortal from '../Profile/UserPortal';
 
 function Test() {
     return (
         <div>
            <TodoPortal />
+           
         </div>
     )
 }
